275ff5b5cdd66009e65896028e35216a892d84a9
Hamafac <borysenko777@gmail.com>
2022-07-09 23:03:43 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+   return (
+      <header className="header">
+         <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/green-company-logo-design-template-e6cda0adebc58ba974fa4d2d02a14d1e_screen.jpg?ts=1567170398" alt="header_company_logo" />
+      </header>
+   );
+}
+
+export default Header
---
+++ src/components/ProfileContentArea.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const ProfileContentArea = () => {
+   return (
+      <div className="content">
+         <div>
+            <img src="https://sp-ao.shortpixel.ai/client/to_webp,q_lossy,ret_img/https://northernhorizon.no/wp-content/uploads/2016/04/02.03.19_NorthernHorizon-35.jpg" alt="main header background" />
+         </div>
+         <div>
+            <img src="https://pictr.com/images/2020/11/16/7uD4Bf.jpg" />
+            description
+         </div>
+         <div>
+            My posts
+            <div>
+               New post
+            </div>
+            <div>
+               post 1
+            </div>
+            <div>
+               post 2
+            </div>
+         </div>
+      </div>
+   );
+}
+
+export default ProfileContentArea
