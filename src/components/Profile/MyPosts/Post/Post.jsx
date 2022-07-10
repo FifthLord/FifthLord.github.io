33541cf2af376141a1daa517934076530dde8478
Hamafac <borysenko777@gmail.com>
2022-07-10 18:52:20 +0300

    improving css

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import s from "./Post.module.css";
+
+const Post = () => {
+   return (
+      <div className={s.item}>
+         <img src="https://aws1.discourse-cdn.com/infiniteflight/original/4X/2/6/3/26318a15ce8a133cc327d0baebbd9dc8ca8c76c1.jpeg" alt="Posts avatar" />
+         post 1
+         <div>
+            <span>Like</span>
+         </div>
+      </div>
+   );
+}
+
+export default Post
