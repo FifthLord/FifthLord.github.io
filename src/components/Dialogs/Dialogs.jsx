7249e7720434cb15bcfe9fe57ac0119aed695ae2
Hamafac <borysenko777@gmail.com>
2022-07-12 23:35:47 +0300

    make massages stile css

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,38 @@ import s from "./Dialogs.module.css";
 
 const Dialogs = () => {
    return (
-      <div>
-         Dialogs
+      <div className={s.dialogs}>
+         <div className={s.dialogsItems}>
+            <div className={s.dialog + ' ' + s.active}>
+               Dimych
+            </div>
+            <div className={s.dialog}>
+               Andrey
+            </div>
+            <div className={s.dialog}>
+               Sveta
+            </div>
+            <div className={s.dialog}>
+               Sacha
+            </div>
+            <div className={s.dialog}>
+               Victor
+            </div>
+            <div className={s.dialog}>
+               Valerar
+            </div>
+         </div>
+         <div className={s.massages}>
+            <div className={s.massage}>
+               Hi
+            </div>
+            <div className={s.massage}>
+               How are u?
+            </div>
+            <div className={s.massage}>
+               yo
+            </div>
+         </div>
       </div>
    )
 }
