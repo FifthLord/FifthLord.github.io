7c15d9342272d69d9a8bdbeb13b4461f6a503a6d
Hamafac <borysenko777@gmail.com>
2022-07-13 23:58:05 +0300

    redact profile module

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,14 +4,17 @@ import Post from "./Post/Post";
 
 const MyPosts = () => {
    return (
-      <div>
-         My posts
-         <div >
-            <textarea></textarea>
-            <button>Add post</button>
-            <button>Remove</button>
+      <div className={s.postsBlock}>
+         <h3>My posts</h3>
+         <div>
+            <div>
+               <textarea></textarea>
+            </div>
+            <div>
+               <button>Add post</button>
+            </div>
          </div>
-         <div className={s.posts}>
+         <div className={s.postsItems}>
             <Post message="Hi, how are you?" likesCount="5" />
             <Post message="It's my first post" likesCount="10" />
          </div>
