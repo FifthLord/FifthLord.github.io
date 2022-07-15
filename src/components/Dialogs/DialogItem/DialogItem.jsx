c42fb03aa03aae37ae816068051b0672809e46db
Hamafac <borysenko777@gmail.com>
2022-07-15 23:29:32 +0300

    add subcomponents dialogs

---
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import s from "./../Dialogs.module.css";
+import { NavLink } from "react-router-dom";
+
+const DialogItem = (props) => {
+   let path = "/dialogs/" + props.id;
+   return (
+      <div>
+         <NavLink to={path} className={dialogData => dialogData.isActive ? s.active : s.dialog} >{props.name}</NavLink>
+      </div>
+   )
+}
+
+export default DialogItem;
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,23 +1,8 @@
 import React from "react";
 import s from "./Dialogs.module.css";
-import { NavLink } from "react-router-dom";
+import Message from "./Message/Message";
+import DialogItem from "./DialogItem/DialogItem";
 
-const DialogItem = (props) => {
-   let path = "/dialogs/" + props.id
-   return (
-      <div>
-         <NavLink to={path} className={dialogData => dialogData.isActive ? s.active : s.dialog} >{props.name}</NavLink>
-      </div>
-   )
-}
-
-const Message = (props) => {
-   return (
-      <div className={s.messages}>
-         {props.message}
-      </div>
-   )
-}
 
 const Dialogs = () => {
 
