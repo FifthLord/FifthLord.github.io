865a2bb3c8aefa959b8c1e1cb04910a8a6b7e2be
Hamafac <borysenko777@gmail.com>
2022-07-18 21:26:20 +0300

    add state file with props arrays fir profile and dialogs

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ const App = (props) => {
             <Navbar />
             <div className="content" >
                <Routes>
-                  <Route path="/dialogs/*" element={<Dialogs dialogs={props.dialogs} messages={props.messages} />} />
-                  <Route path="/profile" element={<Profile posts={props.posts} />} />
+                  <Route path="/dialogs/*" element={<Dialogs dialogsPage={props.state.dialogsPage} />} />
+                  <Route path="/profile" element={<Profile profilePage={props.state.profilePage} />} />
                   <Route path="/news" element={<News />} />
                   <Route path="/music" element={<Music />} />
                   <Route path="/settings" element={<Settings />} />
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -6,10 +6,10 @@ import DialogItem from "./DialogItem/DialogItem";
 
 const Dialogs = (props) => {
 
-   let dialogsElements = props.dialogs
+   let dialogsElements = props.dialogsPage.dialogs
       .map((d) => <DialogItem name={d.name} id={d.id} />);
 
-   let messagesElements = props.messages
+   let messagesElements = props.dialogsPage.messages
       .map((m) => <Message message={m.message} id={m.id} />)
 
    return (
