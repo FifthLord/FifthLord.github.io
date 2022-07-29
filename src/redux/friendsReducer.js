2a9c5e03158510caecdb2aca8bd99c94f98940b8
Hamafac <borysenko777@gmail.com>
2022-07-29 09:46:52 +0300

    fix bug in friends page with reduser initial state

--- src/redux/friendsReducer.js
+++ src/redux/friendsReducer.js
@@ -1,11 +1,23 @@
 
 
 
-let initialState = {};
+let initialState = {
+   friends: [
+      { id: 1, name: 'Dimych' },
+      { id: 2, name: 'Andrey' },
+      { id: 3, name: 'Sveta' },
+      { id: 4, name: 'Sacha' },
+      { id: 5, name: 'Victor' },
+      { id: 6, name: 'Valera' },
+   ],
+};
 
 const friendsReducer = (state = initialState, action) => {
+   switch (action.type) {
 
-   return state;
+      default:
+         return state;
+   }
 }
 
 
