00f313662337484b12563b8dd1be99f638fc54a3
Hamafac <borysenko777@gmail.com>
2022-08-23 23:38:44 +0300

    create ProfileStatus with local state

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 import s from "./Profile.module.css";
 import { getUserProfileThunkCreator } from '../../redux/profileReducer';
 import { useParams } from 'react-router-dom';
-import { withAuthNavigate } from '../../hoc/withAuthNavigate'
+//import { withAuthNavigate } from '../../hoc/withAuthNavigate'
 import { compose } from "redux";
 
 //!используем Хук - UseParams, он позволяет достучаться до url Но так как нельзя хуки и классы мешать, 
@@ -45,5 +45,5 @@ export default compose(
    connect(mapStateToProps, { getUserProfileThunkCreator }),
    withRouter,
    //* HOC is under this line
-   withAuthNavigate
+   //withAuthNavigate
 )(ProfileContainer);
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Preloader from "../../Preloader/Preloader";
 import s from "./ProfileInfo.module.css";
+import ProfileStatus from "./ProfileStatus"
 
 //https://pictr.com/images/2020/11/16/7uD4Bf.jpg
 const ProfileInfo = (props) => {
@@ -8,17 +9,18 @@ const ProfileInfo = (props) => {
       return <Preloader />
    }
 
+   /*<div>
+      <img className={s.titleImage} src="https://sp-ao.shortpixel.ai/client/to_webp,q_lossy,ret_img/https://northernhorizon.no/wp-content/uploads/2016/04/02.03.19_NorthernHorizon-35.jpg"
+         alt="headerBackground" />
+   </div>*/
    return (
       <div>
-         <div>
-            <img className={s.titleImage} src="https://sp-ao.shortpixel.ai/client/to_webp,q_lossy,ret_img/https://northernhorizon.no/wp-content/uploads/2016/04/02.03.19_NorthernHorizon-35.jpg"
-               alt="headerBackground" />
-         </div>
+
          <div className={s.descriptionBlock}>
             <img className={s.myAvatarImage}
                src={props.profile.photos.large}
                alt="userAvatar" />
-            Description
+            <ProfileStatus status={"Hello everyone"} />
          </div>
       </div>
    );
