9f2e970feb4f22aac1fed232185959eb4a83b471
Hamafac <borysenko777@gmail.com>
2022-08-26 23:24:30 +0300

    add redux-form library in login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,53 @@
 import React from "react";
 //import { NavLink } from "react-router-dom";
 import s from "./Login.module.css";
+import { Field, reduxForm } from 'redux-form';
+
+
+const LoginForm = (props) => {
+   return (
+      <form onSubmit={props.handleSubmit}>
+         <div>
+            <Field
+               placeholder={"Login"}
+               name={"login"}
+               component={"input"} />
+         </div>
+         <div>
+            <Field
+               placeholder={"Password"}
+               name={"password"}
+               component={"input"} />
+         </div>
+         <div className={s.checkbox}>
+            <Field
+               component={"input"}
+               name={"rememberMe"}
+               type={"checkbox"} /> remember me
+         </div>
+         <div>
+            <button>Login</button>
+         </div>
+      </form>
+   );
+}
+
+const LoginReduxForm = reduxForm({
+   form: 'login'
+})(LoginForm)
 
 
 const Login = (props) => {
    return (
-      <h1 className={s.login}>
-         LOGIN
-      </h1>
+      <div>
+         <h1 className={s.login}>
+            LOGIN
+         </h1>
+         <LoginReduxForm />
+      </div>
    );
 }
+
 
 export default Login;
 
