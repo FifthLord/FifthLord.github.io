cd0c6100125343be40ee2f8622b1d45d28729b29
Hamafac <borysenko777@gmail.com>
2022-08-29 23:56:17 +0300

    add styles for redux-form field validation

--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -1,9 +1,17 @@
+
 import React from "react"
+import s from "./FormsControls.module.css";
 
 export const Textarea = ({ input, meta, ...props }) => {
+
+   const hasError = meta.touched && meta.error;
+
    return (
-      <div>
-         <textarea {...input} {...props} />
+      <div className={s.formControl + ' ' + (hasError ? s.error : "")}>
+         <div>
+            <textarea {...input} {...props} />
+         </div>
+         {hasError && <span>Some error</span>}
       </div>
    )
 }
