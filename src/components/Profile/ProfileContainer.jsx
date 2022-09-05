b2a091414d478972bc9e45cab43e0fb6803aa0d9
Hamafac <borysenko777@gmail.com>
2022-09-05 23:24:05 +0300

    fix some comments

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,3 +1,5 @@
+
+
 import React from "react";
 import Profile from "./Profile";
 import { connect } from "react-redux";
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -12,34 +12,7 @@ const TOGGLE_IS_FOLLOWING_PROGRESS = 'TOGGLE_IS_FOLLOWING_PROGRESS';
 
 
 let initialState = {
-   users: [
-      /*
-      {
-         id: 1,
-         photoUrl: 'https://www.idlememe.com/wp-content/uploads/2021/11/polite-cat-meme-idlememe-6.jpg',
-         followed: false,
-         name: 'Mykola',
-         status: "I am a boss",
-         location: { city: 'Kyiv', country: 'Ukraine', }
-      },
-      {
-         id: 2,
-         photoUrl: 'https://external-preview.redd.it/t5IBAQ3gaehTHCIiqINnJZ-BOpsXVkRQQ1tzjS0IP84.jpg?auto=webp&s=ad45db84dfdc35658eba374db5f5de34aabbd555',
-         followed: true,
-         name: 'Sasha',
-         status: "I am a boss too",
-         location: { city: 'London', country: 'England', }
-      },
-      {
-         id: 3,
-         photoUrl: 'https://i.pinimg.com/originals/e8/ec/d4/e8ecd48d9f71e8d75c5c7057105afda4.jpg',
-         followed: false,
-         name: 'Andrew',
-         status: "I am a big boss",
-         location: { city: 'Warsaw', country: 'Poland', }
-      },
-*/
-   ],
+   users: [],
    pageSize: 15,
    totalUsersCount: 0,
    currentPage: 1,
