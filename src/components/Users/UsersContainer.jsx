256c7b9bed837e33218f857488022dbb8def6284
Hamafac <borysenko777@gmail.com>
2022-09-17 11:25:00 +0300

    fix some comments

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -61,17 +61,6 @@ let mapStateToProps = (state) => {
    }
 };
 
-/*
-let mapStateToProps = (state) => {
-   return {
-      users: state.usersPage.users,
-      pageSize: state.usersPage.pageSize,
-      totalUsersCount: state.usersPage.totalUsersCount,
-      currentPage: state.usersPage.currentPage,
-      isFetching: state.usersPage.isFetching,
-      followingInProgress: state.usersPage.followingInProgress,
-   }
-};*/
 
 export default compose(
    connect(mapStateToProps, {
